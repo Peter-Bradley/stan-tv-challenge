aac1e60d56b0643f1d0b81f00d3e7693912cb32f
Peter Bradley <pbradley.oz@gmail.com>
2022-03-22 20:24:33 +1100

    Added Basic Routing

--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,18 @@ import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Program from './routes/program'
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path = "/" element={<App />} />
+          <Route path = "program" element={<Program />} />
+        </Routes>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
---
+++ src/routes/program.tsx
@@ -0,0 +1,7 @@
+export default function Program() {
+    return (
+      <main style={{ padding: "1rem 0" }}>
+        <h2>Program</h2>
+      </main>
+    );
+  }
