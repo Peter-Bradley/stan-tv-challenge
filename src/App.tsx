0bc5f2e68b09e132c9a35958a07d6767c277bde1
Peter Bradley <pbradley.oz@gmail.com>
2022-03-22 21:22:01 +1100

    Basic Carosel Setup

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,33 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 6,
+    slidesToSlide: 6 // optional, default to 1.
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 6,
+    slidesToSlide: 6 // optional, default to 1.
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 6,
+    slidesToSlide: 6 // optional, default to 1.
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
+        {/* <Counter />
         <p>
           Edit <code>src/App.tsx</code> and save to reload Routing XDD.
         </p>
@@ -49,8 +69,31 @@ function App() {
           >
             React Redux
           </a>
-        </span>
+        </span> */}
       </header>
+      <Carousel
+      swipeable={false}
+      draggable={false}
+      showDots={true}
+      responsive={responsive}
+      ssr={true} // means to render carousel on server-side.
+      infinite={true}
+      autoPlaySpeed={0}
+      keyBoardControl={true}
+      customTransition="all .5"
+      transitionDuration={500}
+      containerClass="carousel-container"
+      removeArrowOnDeviceType={["tablet", "mobile"]}
+      dotListClass="custom-dot-list-style"
+      itemClass="carousel-item-padding-40-px"
+    >
+      <div>Item 1</div>
+      <div>Item 2</div>
+      <div>Item 3</div>
+      <div>Item 4</div>
+      <div>Item 5</div>
+      <div>Item 6</div>
+    </Carousel>
     </div>
   );
 }
