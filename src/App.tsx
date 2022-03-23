416e6347b2ee35189cb516f9bb4a50137f83c1c7
Peter Bradley <pbradley.oz@gmail.com>
2022-03-23 19:38:20 +1100

    Images now link to program page with correct info

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 import { useSelector } from 'react-redux'
 import { RootState } from "./app/store";
+import { useNavigate } from "react-router-dom";
 
 function App() {
   const programs = useSelector((state: RootState) => state.programs.programList);
-
-  programs.forEach(function (item, index) {
-    console.log(item);
-  });
+  let navigate = useNavigate();
+  const routeChange = (routeId: number) =>{ 
+    let path = `${routeId}`; 
+    navigate(path);
+  }
 
   const programsList = programs.map((program) =>
-  <li><img src={program.image} alt=':('/></li>
+  <li><img key={program.id.toString()} src={program.image} alt=':(' onClick={() => routeChange(program.id)}/></li>
   );
 
   return (
