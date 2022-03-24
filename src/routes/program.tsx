cae4498e1721f89b4897bdd2d2cfd546bb1f9297
Peter Bradley <pbradley.oz@gmail.com>
2022-03-24 23:58:29 +1100

    Backspace returns to Home

--- src/routes/program.tsx
+++ src/routes/program.tsx
@@ -2,11 +2,26 @@ import './program.css';
 import { useParams } from "react-router-dom";
 import { useSelector } from 'react-redux'
 import { RootState } from "../app/store";
+import { useNavigate } from "react-router-dom";
 //import {selectProgramById} from '../features/programs/programsSlice'
 
 export default function Program() {
   let params = useParams();
   const program = useSelector((state: RootState) => state.programs.programList.find(o => (o.id) === Number(params.id)));
+
+  let navigate = useNavigate();
+  const routeChange = () => {
+    let path = "/";
+    navigate(path);
+  }
+
+  document.onkeydown = function (e) {
+    switch (e.key) {
+      case "Backspace":
+        routeChange()
+        break;
+    }
+  }
 
   if(program === undefined)
   {
